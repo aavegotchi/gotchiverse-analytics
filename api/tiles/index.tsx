d4a8f493bb3db45b9f97070f5528e67c3bee63f2
Frank <frank.pfeift@protonmail.com>
2022-08-18 09:30:28 +0200

    test

---
+++ api/tiles/index.tsx
@@ -0,0 +1,39 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { gql } from "@apollo/client";
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import { gotchiverseSubgraphClient } from "../../graph/clients";
+
+interface Tile {
+    name: string;
+    amount: string;
+    uri: string;
+}
+
+type Data = {
+    tileTypes: string[];
+};
+
+export default async function (
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const result = await gotchiverseSubgraphClient.query({
+        query: gql`
+            query fetchAllTiles {
+                tileTypes {
+                    name
+                    amount
+                    uri
+                }
+            }
+        `,
+    });
+
+    res.status(200).json(
+        result.data.tileTypes.map((e: Tile) => ({
+            ...e,
+            amount: parseInt(e.amount),
+        }))
+    );
+}
